06aaf7a4cec670943eb8ff98fda6debb56b15daf
Ndumiso Mthembu <mrnnmthembu@gmail.com>
2021-11-30 14:39:23 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  API_URL: 'https://bukhosiblooms.co.za/api/',
+  // BASE_URL: 'https://bukhosiblooms.co.za/',
+  // API_URL: 'https://bukhosiblooms.co.za//api',
+  // BASE_URL: 'https://bukhosiblooms.co.za/',
+  // API_URL: 'http://localhost:8200/api-tybo-accounting',
+  BASE_URL: 'http://localhost:4200',
+  BASE_URL_ADMIN: 'http://localhost:4200',
+  ACCOUNT_TEST_EMAIL: 'info@bukhosiblooms.co.za',
+  ACCOUNT_TEST_PASSWORD: 'info@bukhosiblooms.co.za',
+  TEST_EMAIL_ACCOUNT: 'admin@tybo-services.co.za',
+  DF_LOGO: `https://bukhosiblooms.co.za/api//api/upload/uploads/1625394631iio.jpg`,
+  DF_USER_LOGO: `https://bukhosiblooms.co.za/api//api/upload/uploads/1625394631iio.jpg`,
+};
